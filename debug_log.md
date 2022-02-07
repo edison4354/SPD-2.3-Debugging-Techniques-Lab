2102f4ca60fedcae6d00d7c280df73e1871ede4f
Edison Li <edison4354@gmail.com>
2022-02-07 15:55:58 -0800

    Exercise 1 - Bug 2-6: Fixed!

--- debug_log.md
+++ debug_log.md
@@ -15,7 +15,32 @@ _Then I noticed another bug ..._
 
 ## Exercise 1
 
-After running the website on my local machine I went through the user flow. I found that once the new order was made the program wasn't showing the pizza order even though it should've. So, I used the trace backward technique by searching the invaild keyword in the code. I discovered the bug on line 79 file in app.py it was caused by an incorrect variable name 'topping' instead of 'topping_type' while creating a object of PizzaTopping.
+```
+TypeError: 'topping' is an invalid keyword argument for PizzaTopping
+```
+
+I found when new order was made the program wasn't showing the pizza order even though it should've. So, I used the trace backward technique by searching the invaild keyword `topping` in the code. I discovered the bug on line 87 file in `app.py` it was caused by an incorrect property name `topping` instead of `topping_type` while creating a object of `PizzaTopping`. Fixed!
+
+```
+werkzeug.routing.BuildError: Could not build url for endpoint '/'. Did you mean 'fulfill_order' instead?
+```
+
+Then I noticed another bug on line 93 in app.py the redirect path returned the error-message above. The bug was caused by using `/` instead of `home`. Changed `redirect(url_for('/'))` to `redirect(url_for('home'))`. Fixed!
+
+```
+sqlalchemy.exc.IntegrityError: (sqlite3.IntegrityError) NOT NULL constraint failed: pizza.order_name
+[SQL: INSERT INTO pizza (order_name, size, crust_type, fulfilled) VALUES (?, ?, ?, ?)]
+[parameters: (None, None, 'THICK', 0)]
+```
+
+New error when attempting to insert a `Pizza` object. In the function `pizza_order_submit` the variable `order_name` on line 76 is `None`. The form input name in `order.html` isn't matching. Changed `order_name = request.form.get('name')` to `order_name = request.form.get('order_name')`. Fixed!
+
+Same problem with `pizza_size_str` on line 77. Changed `pizza_size_str = request.form.get('size')` to `pizza_size_str = request.form.get('pizza_size')`. Fixed!
+
+I noticed that the `toppings` for the pizza object was returning the wrong value. Changed line 79 `toppings_list = request.form.get('toppings')` to  `toppings_list = request.form.getlist('toppings')`.
+Line 86 `for topping_str in ToppingType:` to `for topping_str in toppings_list:`. Fixed!
+
+The last and final bug I found was that the pizza data wasn't commited after being added to the session. Included `db.session.commit()` after `db.session.add(pizza)`. Fixed!
 
 ## Exercise 2
 
--- exercise-1/app.py
+++ exercise-1/app.py
@@ -9,18 +9,21 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 db = SQLAlchemy(app)
 
 ###############################################
-### MODELS
+# MODELS
 ###############################################
+
 
 class PizzaSize(enum.Enum):
     SMALL = '12 Inch'
     MEDIUM = '16 Inch'
     LARGE = '20 Inch'
 
+
 class CrustType(enum.Enum):
     THIN = 'Thin'
     THICK = 'Thick'
     GLUTEN_FREE = 'Gluten Free'
+
 
 class ToppingType(enum.Enum):
     SOY_CHEESE = 'Soy Cheese'
@@ -28,6 +31,7 @@ class ToppingType(enum.Enum):
     ONIONS = 'Onions'
     SPINACH = 'Spinach'
     PINEAPPLE = 'Pineapple'
+
 
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,22 +41,26 @@ class Pizza(db.Model):
     toppings = db.relationship('PizzaTopping')
     fulfilled = db.Column(db.Boolean, default=False)
 
+
 class PizzaTopping(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     topping_type = db.Column(db.Enum(ToppingType))
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
 
+
 with app.app_context():
     db.create_all()
 
 ###############################################
-### ROUTES
+# ROUTES
 ###############################################
+
 
 @app.route('/')
 def home():
     all_pizzas = Pizza.query.filter_by(fulfilled=False)
     return render_template('home.html', pizza_orders=all_pizzas)
+
 
 @app.route('/order', methods=['GET'])
 def pizza_order_form():
@@ -62,26 +70,28 @@ def pizza_order_form():
         crust_types=CrustType,
         toppings=ToppingType)
 
+
 @app.route('/order', methods=['POST'])
 def pizza_order_submit():
-    order_name = request.form.get('name')
-    pizza_size_str = request.form.get('size')
+    order_name = request.form.get('order_name')
+    pizza_size_str = request.form.get('pizza_size')
     crust_type_str = request.form.get('crust_type')
-    toppings_list = request.form.get('toppings')
+    toppings_list = request.form.getlist('toppings')
 
     pizza = Pizza(
         order_name=order_name,
         size=pizza_size_str,
         crust_type=crust_type_str)
-    print(pizza.size)
 
-    for topping_str in ToppingType:
+    for topping_str in toppings_list:
         pizza.toppings.append(PizzaTopping(topping_type=topping_str))
 
     db.session.add(pizza)
+    db.session.commit()
 
     flash('Your order has been submitted!')
-    return redirect(url_for('/'))
+    return redirect(url_for('home'))
+
 
 @app.route('/fulfill', methods=['POST'])
 def fulfill_order():
@@ -91,9 +101,10 @@ def fulfill_order():
     pizza.fulfilled = True
     db.session.add(pizza)
     db.session.commit()
-    
+
     flash(f'The order for {pizza.order_name} has been fulfilled.')
     return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
